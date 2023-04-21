f4a8672cc4455f801109fb1d9e88ac4e2ed18f46
Alexander Zarutskiy <sashasolo23@gmail.com>
2023-04-21 12:24:36 +0100

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Alexander Zarutskiy on 21.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
